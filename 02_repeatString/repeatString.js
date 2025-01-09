5d3fa3230095cc7ed20905d8010c92edfbe42b01
Andrew Camara <andrew.camara@warwick.ac.uk>
2025-01-09 10:15:50 +0000

    Solve exercies 1-3, fix jest Solved exercises 1-3. Although Odin said safe to ignore, I also fixed the reported issues when testing jest, in case I use it in future.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatN) {
+    if (repeatN < 0) return "ERROR";
+    let stringArray = []
+    for (let i = 0; i < repeatN; i++) {
+        stringArray.push(string);
+    }
+   return stringArray.toString().replaceAll(",","");
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = ""
+    for (let i = 1; i < (word.length + 1); i++) {
+        reversedWord += word.at(-i)
+    }
+    return reversedWord
 };
 
 // Do not edit below this line
